0a9bf0fc15f727a1156d7e82bda94f9d5aa6cafa
Pavlov Ivan <pavlov200912@gmail.com>
2020-10-23 10:47:45 +0300

    hw2 done

--- main.py
+++ main.py
@@ -1,112 +1,50 @@
-"""Array class realisation with tuples inside. Education purpose."""
+import functools
 
 
-#  I understand that my documentation is excessive
-#  I wouldn't add so many comments without linter
+class ContractError(Exception):
+    """We use this error when someone breaks our contract."""
 
 
-class Array(object):  # noqa: WPS214 I need more than 7 methods
-    """Class copies list functionality, but with tuples inside."""
+#: Special value, that indicates that validation for this type is not required.
+Any = object()
 
-    def __init__(self, *initial_array):
-        """Array can be initialized with sequence of arguments.
 
-        :param initial_array: object that can be converted to tuple
+def contract(arg_types=None, return_type=None, raises=None):
+    """
+    Creates decorator that checks argument type, return type, raised exceptions restrictions
+
+    :param arg_types: tuple with types of arguments or None, Any considered as common type for all objects
+    :param return_type: type of returned by func value or None if it's not specified
+    :param raises: tuple of Exception inheritors - restrictions on raised by func exceptions or None
+    :return: parametrized decorator
+    """
+    def decorator(func):
         """
-        self._data = tuple(initial_array)  # noqa: WPS110 I need _data variable
-
-    def append(self, elem):
-        """:param elem: object to add in Array."""
-        self._data = self._data + (elem,)  # noqa: WPS110
-
-    def __add__(self, other):
+        :raises: ContactError if return, args type or called error is inconsistent with specified in contract
         """
-        Concatenation of self and other.
+        @functools.wraps(func)
+        def wrapped(*args, **kwargs):
+            if arg_types is not None:
+                arg_values = list(args) + list(kwargs.values())
+                if len(arg_values) != len(arg_types):
+                    raise ContractError("Length of arg_types is inconsistent with function arguments")
+                for arg_value, arg_type in zip(arg_values, arg_types):
+                    if arg_type != Any and not isinstance(arg_value, arg_type):
+                        raise ContractError("Can't match arg_value {0}"
+                                            " of type {1} with type {2}".format(arg_value,
+                                                                                type(arg_value),
+                                                                                arg_type))
+            try:
+                result = func(*args, **kwargs)
+            except Exception as e:
+                if raises is None or isinstance(e, raises):
+                    raise e
+                else:
+                    raise ContractError("Raise of {0} unexpected".format(e.__class__)) from e
+            if return_type is not None and not isinstance(result, return_type):
+                raise ContractError("Expected return type {0} but found {1}".format(return_type, type(result)))
+            return result
 
-        :raises TypeError: if other is not Array instance
-        :param other: must be Array
-        :return: new instance of Array
-        """
-        if not isinstance(other, Array):
-            raise TypeError
-        return Array(*(list(self) + list(other)))
+        return wrapped
 
-    def __len__(self):
-        """:return: length of Array."""
-        return len(self._data)  # noqa: WPS110
-
-    # Linter says 'value' is not appropriate variable name here. I think it is.
-    def index(self, target_value):
-        """
-        Find index of value in Array.
-
-        :param target_value: kek
-        :return: index of element which equal to target_value
-                 or -1 if it's not found.
-        """
-        try:
-            return self._data.index(target_value)  # noqa: WPS110
-        except ValueError:
-            return -1
-
-    def __iter__(self):
-        """:yield: tuple iterator."""
-        yield from self._data
-
-    def __getitem__(self, key_item):
-        """
-         Tuple __getitem__ used. Can raise an exception.
-
-        :param key_item: integer in range(0, len(self))
-        :return: value with key_item index
-        """
-        return self._data[key_item]
-
-    def pop(self, index):
-        """
-        Remove element by index and return it.
-
-        :raises IndexError: if index in not appropriate range
-        :param index: integer in range(0, len(self))
-        :return: popped element of array
-        """
-        # index assumed to be > 0
-        if index >= len(self) or index < 0:
-            raise IndexError
-        value_by_index = self._data[index]
-        self._data = self._data[:index] + self._data[index + 1:]  # noqa:WPS110
-        return value_by_index
-
-    # Linter says 'value' is not appropriate variable name here. I think it is.
-    def remove(self, value_to_remove):
-        """
-        Find first element equals to value_to_remove and pop it by index.
-
-        :raises ValueError: if value_to_remove is not found
-        :param value_to_remove: element of Array
-        :return: removed value
-        """
-        index_to_pop = -1
-        for index, cur_value in enumerate(self._data):
-            if cur_value == value_to_remove:
-                index_to_pop = index
-                break
-        if index_to_pop == -1:
-            raise ValueError
-        return self.pop(index_to_pop)
-
-    def __eq__(self, other):
-        """
-        Object are equals if they are instances of Array with equal tuples.
-
-        :param other: any object
-        :return: boolean
-        """
-        if not isinstance(other, Array):
-            return False
-        if len(other) != len(self):
-            return False
-        return all(
-            self_elem == other_elem
-            for self_elem, other_elem in zip(self, other)
-        )
+    return decorator
